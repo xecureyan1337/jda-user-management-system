71a31f9bee00458c23da719d98b2d30f9645fc70
xecureyan1337 <11227050084@student.uinsgd.ac.id>
2025-07-18 14:50:35 +0700

    fix prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,17 @@
 import { PrismaClient } from '@prisma/client'
 
+declare global {
+  var prisma: PrismaClient | undefined
+}
+
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient()
+export const prisma = globalForPrisma.prisma ?? new PrismaClient({
+  log: ['query', 'info', 'warn', 'error'],
+})
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma
+}
